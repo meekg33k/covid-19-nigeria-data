d815a79ef165aadadf16f0695158e27d6ccd8146
Uduak Eren <sandy3jnr@gmail.com>
2020-04-08 06:52:26 -0700

    Added CORS functionality

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const stateSpreadData = require('./data/covid_nigeria_state_data');
 const totalSpreadData = require('./data/covid_nigeria_total_data');
 
 const serve = ({ port }) => {
 	const app = express();
 	app.use(express.json());
+	app.use(cors());
 
 	app.get('/state', async (_, res) => {
 		res.status(200).json({ data: stateSpreadData });
@@ -16,6 +18,13 @@ const serve = ({ port }) => {
 
 	app.get('/total', async (_, res) => {
 		res.status(200).json({ data: totalSpreadData });
+	});
+
+	app.get('*', async (_, res) => {
+		res.status(200).json({
+			states: stateSpreadData,
+			total: totalSpreadData
+		});
 	});
 
 	return new Promise((resolve, reject) => {
